cbf0c4c61927409e33ed5df7e5e6b68d21b6f4d8
Galym Tlektesov <galym2000gt@gmail.com>
2019-11-29 14:44:53 +0600

    Server

---
+++ TCPServer/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 5000);
+            ServerSocket.Start();
+
+            while (true)
+            {
+                TcpClient client = ServerSocket.AcceptTcpClient();
+                lock (_lock) list_clients.Add(count, client);
+                Console.WriteLine("Someone connected!!");
+
+                Thread t = new Thread(handle_clients);
+                t.Start(count);
+                count++;
+            }
+        }
+
+        public static void handle_clients(object o)
+        {
+            int id = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = list_clients[id];
+
+            while (true)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[2048];
+                int byte_count = stream.Read(buffer, 0, buffer.Length);
+
+                if (byte_count == 0)
+                {
+                    break;
+                }
+
+                string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                broadcast($"{id}: SOS", id);
+                Console.WriteLine($"{id}: " + data);
+            }
+
+            lock (_lock) list_clients.Remove(id);
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        public static void broadcast(string data, int id)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+            lock (_lock)
+            {
+                for (int i = 1; i < list_clients.Count; i++)
+                {
+                    if (i == id)
+                    {
+                        continue;
+                    }
+                    NetworkStream stream = list_clients[i].GetStream();
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+                //foreach (TcpClient c in list_clients.Values)
+                //{
+                //    NetworkStream stream = c.GetStream();
+
+                //    stream.Write(buffer, 0, buffer.Length);
+                //}
+            }
+        }
+    }
+}
